a855c0a7a276ede4b6bd31ab49a35c578d3474c1
Suman Kumar Sinha <suman.sinha@ellenox.com>
2024-06-28 08:33:25 +0530

    added file handling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log(`File is uploaded in cloudinary: ${uploadResult.url}`);
+    return uploadResult;
+  } catch (e) {
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation fails
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
